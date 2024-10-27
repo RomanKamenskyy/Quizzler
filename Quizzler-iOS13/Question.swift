9e18ddaa85ad50cea0cdf019ce4159f4e3701ce2
roman <kamensky.ra@gmail.com>
2024-10-27 23:02:18 +0100

    fix

--- Quizzler-iOS13/Question.swift
+++ Quizzler-iOS13/Question.swift
@@ -2,8 +2,7 @@
 //  Question.swift
 //  Quizzler-iOS13
 //
-//  Created by Роман Каменский on 17.07.2023.
-//  Copyright © 2023 The App Brewery. All rights reserved.
+//  Created by roman
 //
 
 import Foundation
